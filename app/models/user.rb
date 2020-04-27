e408a2c157d353ee07945d9502b0f56ff3c539f5
gobackk1 <k.kwbt@icloud.com>
2020-04-27 09:39:20 +0900

    モデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   has_secure_token
   has_secure_password
 
-  validates :email, presence: true ,uniqueness: {case_sensitive: true}
-  validates :name, presence: true
-  validates :password_digest, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {in: 6..50}
+  validates :email, presence: true ,uniqueness: {case_sensitive: true}, length: {maximum: 255}, format: {with: VALID_EMAIL_REGEX}
+  validates :password_digest, presence: true, length: {in: 6..50}
   validates :token, uniqueness: true
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
