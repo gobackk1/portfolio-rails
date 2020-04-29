93110f63d85cb095b589c4eabebd2dcd6b5f8400
gobackk1 <k.kwbt@icloud.com>
2020-04-29 14:04:47 +0900

    勉強機能CRUD操作

---
+++ app/controllers/study_records_controller.rb
@@ -0,0 +1,47 @@
+class StudyRecordsController < ApplicationController
+  before_action :current_user
+  def index
+    @records = StudyRecord.all
+    render json: @records
+  end
+
+  def show
+    @record = StudyRecord.find(params[:id])
+    render json: @record
+  end
+
+  def create
+    @record = StudyRecord.new(study_record_params)
+    @record.user_id = @current_user.id
+
+    if @record.save
+      render json: @record
+    else
+      render json: {errors: @record.errors.full_message}, status: 400
+    end
+  end
+
+  def update
+    @record = StudyRecord.find(params[:id])
+    @record.update_attributes(
+      comment: params[:comment],
+      teaching_material: params[:teaching_material],
+      study_hours: params[:study_hours]
+    )
+    if @record.save
+      render json: @record
+    else
+      render json: {errors: @record.errors.full_message}, status: 400
+    end
+  end
+
+  def destroy
+    StudyRecord.find(params[:id]).destroy
+    render json: params[:id]
+  end
+
+  private
+    def study_record_params
+      params.permit(:comment, :teaching_material, :study_hours)
+    end
+end
